79f67023dc6dffefe48183f197450f5aa8440ef5
hslater <hamish.slater@hallowelt.com>
2025-05-06 20:55:17 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: Id04c382fbf6d6c2bcf1ec1514d6bb705903d05b3
    (cherry picked from commit d28bf348f5809e25d07b7ea0a27652df9ea1fb93)

--- resources/bs.tgcld/util/tag/TagCloudDefinition.js
+++ resources/bs.tgcld/util/tag/TagCloudDefinition.js
@@ -5,17 +5,17 @@ bs.tgcld.util.tag.TagCloudDefinition = function BsVecUtilTagCloudDefinition() {
 
 OO.inheritClass( bs.tgcld.util.tag.TagCloudDefinition, bs.vec.util.tag.Definition );
 
-bs.tgcld.util.tag.TagCloudDefinition.prototype.getCfg = function() {
-	var cfg = bs.tgcld.util.tag.TagCloudDefinition.super.prototype.getCfg.call( this );
-	return $.extend( cfg, {
-		classname : 'TagCloud',
+bs.tgcld.util.tag.TagCloudDefinition.prototype.getCfg = function () {
+	const cfg = bs.tgcld.util.tag.TagCloudDefinition.super.prototype.getCfg.call( this );
+	return $.extend( cfg, { // eslint-disable-line no-jquery/no-extend
+		classname: 'TagCloud',
 		name: 'tagCloud',
 		tagname: 'bs:tagcloud',
 		icon: 'bluespice',
 		descriptionMsg: 'bs-tagcloud-tag-tagcloud-desc',
 		menuItemMsg: 'bs-tagcloud-tag-tagcloud-title',
 		tabbed: true,
-		attributes: [{
+		attributes: [ {
 			name: 'renderer',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-renderer-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-renderer-help',
@@ -27,7 +27,7 @@ bs.tgcld.util.tag.TagCloudDefinition.prototype.getCfg = function() {
 				{ data: 'list', label: mw.message( 'bs-tagcloud-tag-tagcloud-attr-renderer-option-list' ).plain() },
 				{ data: 'canvas3d', label: mw.message( 'bs-tagcloud-tag-tagcloud-attr-renderer-option-canvas3d' ).plain() }
 			]
-		},{
+		}, {
 			name: 'store',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-store-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-store-help',
@@ -38,45 +38,45 @@ bs.tgcld.util.tag.TagCloudDefinition.prototype.getCfg = function() {
 				{ data: 'category', label: mw.message( 'bs-tagcloud-tag-tagcloud-attr-store-option-category' ).plain() },
 				{ data: 'searchstats', label: mw.message( 'bs-tagcloud-tag-tagcloud-attr-store-option-searchstats' ).plain() }
 			]
-		},{
+		}, {
 			name: 'width',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-width-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-width-help',
 			type: 'percent',
 			default: '100',
 			tab: 'common'
-		},{
+		}, {
 			name: 'showcount',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-showcount-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-showcount-help',
 			type: 'toggle',
 			default: true,
 			tab: 'common'
-		},{
+		}, {
 			name: 'minsize',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-minsize-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-minsize-help',
 			type: 'number',
 			default: 5,
 			tab: 'advanced'
-		},{
+		}, {
 			name: 'maxsize',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-maxsize-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-maxsize-help',
 			type: 'number',
 			default: 30,
 			tab: 'advanced'
-		},{
+		}, {
 			name: 'exclude',
 			labelMsg: 'bs-tagcloud-tag-tagcloud-attr-exclude-label',
 			helpMsg: 'bs-tagcloud-tag-tagcloud-attr-exclude-help',
 			type: 'text',
 			default: '',
 			tab: 'advanced'
-		}]
-	});
+		} ]
+	} );
 };
 
 bs.vec.registerTagDefinition(
-	 new bs.tgcld.util.tag.TagCloudDefinition()
+	new bs.tgcld.util.tag.TagCloudDefinition()
 );
